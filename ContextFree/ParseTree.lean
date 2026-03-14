16a6519b8f29e9f03e32b6391b6196ed508fe873
Shaazib Tanvir <syed2302995@st.jmi.ac.in>
2026-03-14 22:00:38 +0530

    feat: mutual commit for rollback maybe

--- ContextFree/ParseTree.lean
+++ ContextFree/ParseTree.lean
@@ -60,7 +60,7 @@ mutual
     (forest : Forest g roots s₂) : Forest g ((.nonterminal n) :: roots) (s₁ ++ s₂)
 end
 
-variable {g : ContextFreeGrammar T} {n : g.NT} {s : List T}
+variable {g : ContextFreeGrammar T} {n : g.NT} {s : List T} {roots : List (Symbol T g.NT)}
 
 def Tree.children (tree : Tree g n s) : List (Symbol T g.NT) :=
   match tree with
@@ -78,9 +78,15 @@ def TreeForest.induction.{u} :=
     (fun {roots : List (Symbol T g.NT)} {s : List T} (t : g.Forest roots s) =>
       @Forest.rec T g motive1 motive2 node empty terminal nonterminal roots s t)
 
-private lemma tree_forest_derives_leftmost_leaves
-  {g : ContextFreeGrammar T}
-  :
+/- def TreeForest.rec := -/
+/-   fun {T} {g : ContextFreeGrammar T} {motive1 motive2} (node empty terminal nonterminal) => -/
+/-     Prod.mk -/
+/-     (fun {n : g.NT} {s : List T} (t : g.Tree n s) => -/
+/-       @Tree.rec T g motive1 motive2 node empty terminal nonterminal n s t) -/
+/-     (fun {roots : List (Symbol T g.NT)} {s : List T} (t : g.Forest roots s) => -/
+/-       @Forest.rec T g motive1 motive2 node empty terminal nonterminal roots s t) -/
+
+private lemma tree_forest_derives_leftmost_leaves :
   (∀ n : g.NT, ∀ s : List T, ∀ _ : Tree g n s,
   g.DerivesLeftmost [.nonterminal n] <| s.map Symbol.terminal)
   ∧
@@ -102,19 +108,69 @@ private lemma tree_forest_derives_leftmost_leaves
       · exact h_n 
 
 theorem Forest.derives_leftmost_leaves
-  {g : ContextFreeGrammar T}
-  {roots : List (Symbol T g.NT)}
-  {s : List T}
   (forest : Forest g roots s)
   : g.DerivesLeftmost roots <| s.map Symbol.terminal := by
     apply tree_forest_derives_leftmost_leaves.right roots s forest
 
 theorem Tree.derives_leftmost_leaves
-  {g : ContextFreeGrammar T}
-  {n : g.NT}
-  {s : List T}
   (tree : Tree g n s)
   : g.DerivesLeftmost [.nonterminal n] <| s.map Symbol.terminal := by
     apply tree_forest_derives_leftmost_leaves.left n s tree
+
+/- private lemma tree_forest_of_derives -/
+/-   : (∀ n : g.NT, ∀ s : List T, ∀ _ : Tree g n s, -/
+/-     g.DerivesLeftmost [.nonterminal n] <| s.map Symbol.terminal) -/
+/-     ∧ -/
+/-     (∀ roots : List (Symbol T g.NT), ∀ s : List T, ∀ _ : Forest g roots s, -/
+/-     g.DerivesLeftmost roots <| s.map Symbol.terminal) -/
+
+def Forest.mkTerminals (ts : List T) : Forest g (ts.map Symbol.terminal) ts :=
+  match ts with
+  | [] =>
+    Forest.empty
+  | t :: ts =>
+    by
+      let forest := mkTerminals ts
+      apply Forest.terminal t forest
+
+mutual
+noncomputable def Forest.of_derives
+  {T : Type u_1} {g : ContextFreeGrammar T} {s : List T}
+  {roots : List (Symbol T g.NT)}
+  (h_derives : g.Derives roots <| s.map Symbol.terminal) : Forest g roots s :=
+  have h_cases := h_derives.cases_head
+  match Classical.dec (roots = List.map Symbol.terminal s) with
+  | .isTrue h_terminal =>
+    by
+      rw [h_terminal]
+      apply Forest.mkTerminals s
+  | .isFalse h_not_terminal =>
+    by
+      have h_trans :
+      ∃ c, g.Produces roots c ∧ g.Derives c (List.map Symbol.terminal s) := by grind
+      let c := h_trans.choose
+      have hc_eq : c = h_trans.choose := by rfl
+      have hc := by apply h_trans.choose_spec
+      rw [←hc_eq] at hc
+      clear hc_eq
+      obtain ⟨ h_produces, h_derives' ⟩ := hc
+      let forest := Forest.of_derives h_derives'
+      sorry
+termination_by h_derives
+
+noncomputable def Tree.of_derives
+  {T : Type u_1} {g : ContextFreeGrammar T} {n : g.NT} {s : List T}
+  (h_derives : g.Derives [.nonterminal n] <| s.map Symbol.terminal)
+  : Tree g n s :=
+  have h_cases :
+  ∃ c, g.Produces [Symbol.nonterminal n] c
+  ∧ g.Derives c (List.map Symbol.terminal s) := by
+    have h := h_derives.cases_head
+    rcases h with h | h
+    · cases s with grind
+    · exact h
+  have children := h_cases.choose
+  Tree.node children (by sorry) (sorry)
+end
 
 end ContextFreeGrammar
